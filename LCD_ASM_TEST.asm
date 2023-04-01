e7f7bc88f670c5359c6f661ea45aaec360aba4d3
kiwan:p <35855668+rldhkstopic@users.noreply.github.com>
2023-04-01 23:35:59 +0900

    성공했다 인간승리..

--- LCD_ASM_TEST.asm
+++ LCD_ASM_TEST.asm
@@ -26,6 +26,12 @@
 		CALL LCD_CURSUR
 	.ENDMACRO
 
+	.MACRO LCD_COMM
+        STS LCD_INST_PTR, @0
+        CALL D5MS
+	.ENDMACRO
+		
+
 .CSEG
     .ORG 0x0000
         RJMP LOOP
@@ -44,47 +50,37 @@
         LCD_POS 0, 0
         LCD_CHARS 'L'
 
-		LCD_POS 0, 1
+		LCD_POS 1, 0
         LCD_CHARS 'O'
 
-		LCD_POS 0, 2
+		LCD_POS 0, 1
         LCD_CHARS 'V'
 
-		LCD_POS 1, 2
+		LCD_POS 1, 1
         LCD_CHARS 'E'
 
     INFINITE:
         RJMP INFINITE    
 
-    LCD_INIT: ; void
+    LCD_INIT:
         LDI CHD, 0x38
-        CALL LCD_COMM   ; LCD_comm(0x38)
+        LCD_COMM CHD   ; LCD_comm(0x38)
         LDI CHD, 0x0D
-        CALL LCD_COMM   ; LCD_comm(0x0C)
+        LCD_COMM CHD   ; LCD_comm(0x0C)
         LDI CHD, 0x06
-        CALL LCD_COMM   ; LCD_comm(0x06)
-        
+        LCD_COMM CHD   ; LCD_comm(0x06)        
         LDI CHD, 0x01
-        CALL LCD_COMM   ; LCD_clear() : LCD_comm(1)
+        LCD_COMM CHD   ; LCD_clear() : LCD_comm(1)
 
         RET
 
-	LCD_DATAIN: ; char ch
-        CALL LCD_DATA
+	LCD_DATAIN:
+        STS LCD_DATA_PTR, CHE
         CALL D50US
         RET
 
-    LCD_COMM: ; char ch
-        STS LCD_INST_PTR, CHD
-        CALL D5MS
-        RET
-
-    LCD_DATA:
-        STS LCD_DATA_PTR, CHE
-		RET
-
     LCD_CHAR:
-        CALL LCD_DATA
+        STS LCD_DATA_PTR, CHE
         CALL D50US
         RET
     
@@ -94,16 +90,18 @@
         PUSH R18  
 		PUSH R19
 
-        MOV R17, COL    ; move COL to R17
-        MOV R18, ROW    ; move ROW to R18
-		LDI R19, 0x40
+		MOV R17, COL ; move COL to R17
+		MOV R18, ROW ; move ROW to R18
+		LSL R18
+		LSL R18
+		LSL R18
+		LSL R18
+		LSL R18
+		LSL R18
 
-        MUL R18, R19    ; R18 <- ROW * 0x40
         ADD R17, R18    ; R17 <- COL + ROW * 0x40
         ORI R17, 0x80   ; R17 <- 0x80|(COL + ROW * 0x40)
-
-        MOV CHD, R17    ; CH <- R17
-        CALL LCD_COMM   ; call LCD_COMM with CH in R16
+        LCD_COMM R17    ; 
         
 		POP R19
         POP R18      
